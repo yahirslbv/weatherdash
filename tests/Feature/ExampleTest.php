6dd8dbf9fefb1692919574a83b9d9ad774dd45bd
yahirslbv <victor.barrera@uabc.edu.mx>
2026-05-19 20:45:01 -0700

    12vo

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,7 +2,6 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
@@ -14,6 +13,8 @@ class ExampleTest extends TestCase
     {
         $response = $this->get('/');
 
-        $response->assertStatus(200);
+        // Cambiamos a 302 porque tu ruta raíz hace un redirect a /home
+        $response->assertStatus(302);
+        $response->assertRedirect('/home');
     }
 }
